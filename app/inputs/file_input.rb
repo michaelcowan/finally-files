e757ab5d3f1ada0a1cf264f36b5a8357e0f4653c
Mike Cowan <michaelcowan@users.noreply.github.com>
2015-02-21 22:52:42 +0000

    Added input_html support

--- app/inputs/file_input.rb
+++ app/inputs/file_input.rb
@@ -1,13 +1,14 @@
 class FileInput < SimpleForm::Inputs::FileInput
   # http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3/
   def input(wrapper_options=nil)
-    button = template.content_tag(:div, class: 'input-group') do
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+    template.content_tag(:div, class: 'input-group') do
       (template.content_tag(:span, class: 'input-group-btn') do
         template.content_tag(:span, class: 'btn btn-primary btn-file') do
-          @builder.file_field(attribute_name) + raw('Browse&hellip;')
+          @builder.file_field(attribute_name, merged_input_options) + raw('Browse&hellip;')
         end
       end) +
-      template.tag(:input, class: 'form-control', type: 'text', readonly: true)
+      template.tag(:input, input_html_options.merge({class: 'form-control', type: 'text'}))
     end
   end
 end
